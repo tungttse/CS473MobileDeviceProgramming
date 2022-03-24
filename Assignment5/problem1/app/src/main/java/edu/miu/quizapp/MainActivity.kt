fee375ae7c16bf58b2b0af92cc5cf2134cde486a
TungTop <tungttse@gmail.com>
2022-03-23 22:17:48 -0500

    update assignment 5 - problem 1

--- Assignment5/problem1/app/src/main/java/edu/miu/quizapp/MainActivity.kt
+++ Assignment5/problem1/app/src/main/java/edu/miu/quizapp/MainActivity.kt
@@ -5,14 +5,12 @@ import android.os.Bundle
 import android.app.AlertDialog
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.RadioButton
 import android.widget.RadioGroup
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var radioGroup
-            : RadioGroup? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,37 +18,44 @@ class MainActivity : AppCompatActivity() {
         val submitBtn = findViewById<Button>(R.id.submitBtn)
         val resetBtn = findViewById<Button>(R.id.resetBtn)
 
-        showDialog("You submitted on 2022-03-14 Your achieved 50%");
+        var result = 0;
 
-//        var result = 0;
-//        radioGroup = findViewById<RadioButton>(R.id.myRadioGroup);
-//        if(radioGroup!=null) {
-//            radioGroup.setOnCheckedChangeListener { radioGroup, i ->
-//                when (radioGroup.getCheckedRadioButtonId()) {
-//                    R.id.a1 -> result = result + 50;
-//                    R.id.a2 -> result = result
-//                    else ->  result = result
-//                }
-//            }
-//        }
-//
-//        var checkBox:CheckBox = CheckBox(context)
-//        checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
-//            if (isChecked) {
-//                result = result + 50;
-//            }
-//        }
-//
-//        submitBtn.setOnClickListener {
-//            val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
-//            val currentDate = sdf.format(Date())
-//            showDialog("“Congratulations! You submitted on " + currentDate + " Your achieved " + result + "%");
-//        }
-//
-//        resetBtn.setOnClickListener {
-//            radioGroup.clearCheck();
-//            result = 0;
-//        }
+        var radioGroup = findViewById<RadioGroup>(R.id.groupRadio);
+        if(radioGroup!=null) {
+            radioGroup.setOnCheckedChangeListener { radioGroup, i ->
+                when (i) {
+                    R.id.a1 -> result = result + 50;
+                    R.id.a2 -> result = result
+                    else ->  result = result
+                }
+            }
+        }
+
+        var checkBox1:CheckBox =  findViewById<CheckBox>(R.id.c1);
+        var checkBox2:CheckBox =  findViewById<CheckBox>(R.id.c2);
+        var checkBox3:CheckBox =  findViewById<CheckBox>(R.id.c3);
+        var checkBox4:CheckBox =  findViewById<CheckBox>(R.id.c4);
+
+        checkBox1.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                result = result + 50;
+            }
+        }
+
+        submitBtn.setOnClickListener {
+            val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentDate = sdf.format(Date())
+            showDialog("You submitted on " + currentDate + " Your achieved " + result.toString() + "%");
+        }
+
+        resetBtn.setOnClickListener {
+            radioGroup?.clearCheck();
+            result = 0;
+            checkBox1.isChecked = false;
+            checkBox2.isChecked = false;
+            checkBox3.isChecked = false;
+            checkBox4.isChecked = false;
+        }
     }
 
     fun showDialog(message: String) {
